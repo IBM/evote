c60cd4321239ad2ee572d1f34ec968d9d5ce6869
Horea Porutiu <horeaporutiu@gmail.com>
2019-06-13 18:29:05 -0700

    added election error checking

--- contract/lib/my-asset-contract.js
+++ contract/lib/my-asset-contract.js
@@ -19,23 +19,21 @@ const ballotDataPath = path.join(process.cwd(), './lib/data/ballotData.json');
 const ballotDataJson = fs.readFileSync(ballotDataPath, 'utf8');
 const ballotData = JSON.parse(ballotDataJson);
 
-// const firstChoice = 0;
-// const secondChoice = 1;
-
-// let util = require('util');
-
 //import our file which contains our constructors and auxiliary function
 let Ballot = require('./Ballot.js');
 let Election = require('./Election.js');
 let Voter = require('./Voter.js');
 let VotableItem = require('./VotableItem.js');
+
 let HelperFunctions = require('./HelperFunctions.js');
 let helperFunctions = new HelperFunctions();
+let Query = require('./query.js');
+let query = new Query();
 
+const util = require('util');
 
 class MyAssetContract extends Contract {
 
-  
   /**
    *
    * init
@@ -55,12 +53,18 @@ class MyAssetContract extends Contract {
     let voters = [];
     let votableItems = [];
     let elections = [];
+    let election;
 
-    //create FNRJ
+    //create voters
     let voter1 = await new Voter('V1', '234', 'Horea', 'Porutiu');
     let voter2 = await new Voter('V2', '345', 'Duncan', 'Conley');
     let voter3 = await new Voter('V3', '456', 'Mark', 'Ashla');
     let voter4 = await new Voter('V4', '567', 'Danny', 'Powell');
+    //update voters array
+    voters.push(voter1);
+    voters.push(voter2);
+    voters.push(voter3);
+    voters.push(voter4);
 
     //add the voters to the world state, the election class checks for registered voters 
     await helperFunctions.updateMyAsset(ctx, voter1.voterId, voter1);
@@ -68,25 +72,31 @@ class MyAssetContract extends Contract {
     await helperFunctions.updateMyAsset(ctx, voter3.voterId, voter3);
     await helperFunctions.updateMyAsset(ctx, voter4.voterId, voter4);
 
-    //election day is always on a tuesday, and lasts a full day
-    let electionStartDate = await new Date(2020, 11, 3);
-    let electionEndDate = await new Date(2020, 11, 4);
+    //query for election first before creating one.
+    let currElections = JSON.parse(await query.queryByObjectType(ctx, 'election'));
+    console.log(util.inspect('currElections: '));
+    console.log(util.inspect(currElections));
 
-    //create the election
-    let election = await new Election(electionData.electionName, electionData.electionCountry,
-      electionData.electionYear, electionStartDate, electionEndDate);
+    if (currElections.length === 0) {    
 
-    //update voters array
-    voters.push(voter1);
-    voters.push(voter2);
-    voters.push(voter3);
-    voters.push(voter4);
 
-    //update elections array
-    elections.push(election);
-    console.log(`***************************************************
-      election.electionId: ${election.electionId} and election: ${election}`);
-    await helperFunctions.updateMyAsset(ctx, election.electionId, election);
+      //create the election
+      //election day is always on a tuesday, and lasts a full day
+      let electionStartDate = await new Date(2020, 11, 3);
+      let electionEndDate = await new Date(2020, 11, 4);
+      election = await new Election(electionData.electionName, electionData.electionCountry,
+        electionData.electionYear, electionStartDate, electionEndDate);
+      console.log('util inspect voters: ');
+      console.log(util.inspect(voters));
+  
+      //update elections array
+      elections.push(election);
+      console.log(`***************************************************
+        election.electionId: ${election.electionId} and election: ${election}`);
+      await helperFunctions.updateMyAsset(ctx, election.electionId, election);
+    } else {
+      election = currElections[0];
+    }
 
     //create votableItems for the ballots
     let presVotable = await new VotableItem(ctx, 'VI1', ballotData.presidentialRaceTitle,
@@ -106,10 +116,6 @@ class MyAssetContract extends Contract {
 
     //save choices in world state
     for (let i = 0; i < votableItems.length; i++) {
-
-      console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& votable items &&&&&&&&&&&&&&&&&&');
-      console.log('votableItems[i]');
-      console.log(votableItems[i]);
       await helperFunctions.updateMyAsset(ctx, votableItems[i].votableId, votableItems[i]);
     }
 
@@ -118,25 +124,21 @@ class MyAssetContract extends Contract {
 
       if (!voters[i].ballot) {
 
+        console.log('inside !voters[i].ballot');
+
         //give each registered voter a ballot
         voters[i].ballot = await new Ballot(ctx, votableItems, election, voters[i].voterId);
         voters[i].ballotCreated = true;
 
         //update state with ballots
         await helperFunctions.updateMyAsset(ctx, voters[i].ballot.ballotId, voters[i].ballot);
-
+        await helperFunctions.updateMyAsset(ctx, voters[i].voterId, voters[i]);
       } else {
         console.log('these voters already have ballots');
         break;
       }
 
     }
-
-    //update the voters with their ballots before they vote.
-    await helperFunctions.updateMyAsset(ctx, voter1.voterId, voter1);
-    await helperFunctions.updateMyAsset(ctx, voter2.voterId, voter2);
-    await helperFunctions.updateMyAsset(ctx, voter3.voterId, voter3);
-    await helperFunctions.updateMyAsset(ctx, voter4.voterId, voter4);
 
     return voters;
 
